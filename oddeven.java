5544d62b2d9daa58c0f1301e32f8ef0a855be8e0
keshav_rajput18 <codewithkeshav18@gmail.com>
2023-08-05 10:20:05 +0530

    solved

---
+++ oddeven.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class oddeven {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a%2==0){
+            System.out.println("Number is EVEN");
+        }else{
+            System.out.println("Number is ODD");
+        }
+    }
+}
