7d1b99982129d4532ea49a389e34c0a561340941
keshav_rajput18 <codewithkeshav18@gmail.com>
2023-08-05 10:50:17 +0530

    use try for sc error

--- income_tax_calculator.java
+++ income_tax_calculator.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class income_tax_calculator{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int income = sc.nextInt();
-        if(income<500000){
-            System.out.println("0% Tax");
-        }else if(500000<income && income<1000000){
-            System.out.println("20% Tax");
-        }else{
-            System.out.println("30% Tax");
+        try (Scanner sc = new Scanner(System.in)) {
+            int income = sc.nextInt();
+            if(income<500000){
+                System.out.println("0% Tax");
+            }else if(500000<income && income<1000000){
+                System.out.println("20% Tax");
+            }else{
+                System.out.println("30% Tax");
+            }
         }
     }
 }
--- oddeven.java
+++ oddeven.java
@@ -1,13 +1,14 @@
 import java.util.*;
 
 public class oddeven {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        if(a%2==0){
-            System.out.println("Number is EVEN");
-        }else{
-            System.out.println("Number is ODD");
+    public static void main(String args[]){
+        try (Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            if(a%2==0){
+                System.out.println("Number is EVEN");
+            }else{
+                System.out.println("Number is ODD");
+            }
         }
     }
 }
